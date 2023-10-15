3abcc0e6e552771c066384c7b9c5445a4ad1d14a
Vincent Dinh <vincentt.dinh03@gmail.com>
2023-10-15 13:44:24 -0500

    added documentation for classes and methods

--- Titanic-Project/src/main/java/com/core/TravelAgent.java
+++ Titanic-Project/src/main/java/com/core/TravelAgent.java
@@ -1,8 +1,25 @@
 package com.core;
 
+/**
+ * Representation of a travel agent in the cruise reservation system.
+ *
+ * This class provides specific functionalities and attributes related to a travel agent who assists guests in cruise booking and other related services.
+ *
+ * @author Your Name (replace this with your actual name)
+ * @version 1.0
+ * @see User
+ */
 public class TravelAgent extends User {
 
-
+    /**
+     * Constructor for creating a new Travel Agent.
+     *
+     * @param username   The username of the travel agent.
+     * @param password   The password of the travel agent.
+     * @param id         The unique ID of the travel agent.
+     * @param firstName  The first name of the travel agent.
+     * @param lastName   The last name of the travel agent.
+     */
     public TravelAgent(String username, String password, Long id, String firstName, String lastName) {
         super(username, password, id, firstName, lastName);
     }
--- Titanic-Project/src/main/java/com/core/User.java
+++ Titanic-Project/src/main/java/com/core/User.java
@@ -1,5 +1,13 @@
 package com.core;
 
+/**
+ * Abstract representation of a user in the cruise reservation system.
+ *
+ * This class provides a basic blueprint for all types of users in the system like Guest, Travel Agent, and Admin.
+ *
+ * @author Your Name (replace this with your actual name)
+ * @version 1.0
+ */
 public abstract class User {
     private String username;
     private String password;
@@ -7,6 +15,15 @@ public abstract class User {
     private String firstName;
     private String lastName;
 
+    /**
+     * Constructor for creating a new User.
+     *
+     * @param username   The username of the user.
+     * @param password   The password of the user.
+     * @param id         The unique ID of the user.
+     * @param firstName  The first name of the user.
+     * @param lastName   The last name of the user.
+     */
     public User(String username, String password, Long id, String firstName, String lastName) {
         this.username = username;
         this.password = password;
@@ -14,11 +31,4 @@ public abstract class User {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
 }
-
-
-
-
-
-
