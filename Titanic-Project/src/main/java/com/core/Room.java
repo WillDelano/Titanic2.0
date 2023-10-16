b9e6d840da73433cee43185027d0f8eece08bf75
cole hogan <cole_hogan1@baylor.edu>
2023-10-16 14:39:23 -0500

    added comments to the Room class

--- Titanic-Project/src/main/java/com/core/Room.java
+++ Titanic-Project/src/main/java/com/core/Room.java
@@ -23,52 +23,105 @@ public class Room {
 
 
 
-
+    /**
+     * default constructor of the Room
+     *
+     * @param
+     * @param
+     */
     public Room(){
         numberOfBeds = 0;
         bedType = 0;
 
     }
+    /**
+     * sets the Room number/ id
+     *
+     * @param roomNumber
+     */
     public void setRoomNumber(Integer roomNumber) {
         this.roomNumber = roomNumber;
     }
+    /**
+     * sets the NumberOfBeds in a room
+     *
+     * @param numberOfBeds
+     */
 
     public void setNumberOfBeds(Integer numberOfBeds) {
         this.numberOfBeds = numberOfBeds;
     }
-
+    /**
+     * Sets the type of Bed in a room
+     *
+     * @param bedType
+     */
     public void setBedType(Integer bedType) {
         this.bedType = bedType;
     }
-
+    /**
+     * sets the smoking boolean
+     *
+     * @param smokingAvailable
+     */
     public void setSmokingAvailable(Boolean smokingAvailable) {
         this.smokingAvailable = smokingAvailable;
     }
-
-    public void setRoomPrice(Double roomPrice) {
-        this.roomPrice = roomPrice;
+    /**
+     * After all the room information is set, this method
+     * will take the number of beds, and the types, the room type,
+     * to set the room price.
+     *
+     * @param
+     */
+    public void setRoomPrice() {
+        //Here the
     }
-
+    /**
+     * retrieves the Room Number
+     *
+     * @param
+     */
     public Integer getRoomNumber() {
         return roomNumber;
     }
-
+    /**
+     * retrieves the number of Beds
+     *
+     * @param
+     */
     public Integer getNumberOfBeds() {
         return numberOfBeds;
     }
-
+    /**
+     * retrives the bed type
+     *
+     * @param
+     */
     public Integer getBedType() {
         return bedType;
     }
-
+    /**
+     * retrives the smoking availability
+     *
+     * @param
+     */
     public Boolean getSmokingAvailable() {
         return smokingAvailable;
     }
-
+    /**
+     * retrieves the Room price
+     *
+     * @param
+     */
     public Double getRoomPrice() {
         return roomPrice;
     }
-
+    /**
+     * overrtides the equals operation to aid in comparing Rooms with eachother
+     *
+     * @param object: the room being compared with this
+     */
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
@@ -76,7 +129,11 @@ public class Room {
         Room room = (Room) object;
         return java.util.Objects.equals(numberOfBeds, room.numberOfBeds) && java.util.Objects.equals(bedType, room.bedType) && java.util.Objects.equals(smokingAvailable, room.smokingAvailable) && java.util.Objects.equals(roomPrice, room.roomPrice);
     }
-
+    /**
+     * overrides the hashing of the rooms, allowing the system to put rooms in maps and sets
+     *
+     * @param
+     */
     public int hashCode() {
         return Objects.hash(super.hashCode(), numberOfBeds, bedType, smokingAvailable, roomPrice);
     }
