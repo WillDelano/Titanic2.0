c95a19291cee58ec5b4b0f9d3e3f171969513454
eeggssyy <118235620+eeggssyy@users.noreply.github.com>
2023-10-18 23:07:22 -0500

    Added comments to id class

--- Titanic-Project/src/main/java/com/core/uniqueID/UniqueID.java
+++ Titanic-Project/src/main/java/com/core/uniqueID/UniqueID.java
@@ -1,13 +1,33 @@
 package com.core.uniqueID;
 
+import com.core.users.User;
+
+/**
+ * Representation of an admin user in the cruise reservation system.
+ *
+ * This class provides specific functionalities and attributes related to an admin user responsible for managing system settings, travel agent accounts, etc.
+ *
+ * @author Vincent Dinh
+ * @version 1.0
+ * @see User
+ */
 public class UniqueID {
     private static int uniqueNumber = 0;
     private int id;
 
+    /**
+     * Constructor for creating a new Guest.
+     *
+     */
     public UniqueID() {
         id = uniqueNumber;
     }
 
+    /**
+     * Constructor for creating a new Guest.
+     *
+     * @return The unique id
+     */
     public int getId() {
         return id;
     }
