cea6fc3801dcde929d901617769a438cf08a7c6f
Vincent Dinh <vincentt.dinh03@gmail.com>
2023-10-21 17:22:19 -0500

    added cruise details page highlighting sample cruise data

---
+++ Titanic-Project/src/main/java/edu/ui/cruiseDetails/CruiseDetailsPage.java
@@ -0,0 +1,53 @@
+package edu.ui.cruiseDetails;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CruiseDetailsPage {
+
+    private JFrame detailsFrame;
+    private JLabel cruiseLabel;
+    private JTextArea detailsTextArea;
+    private JButton selectRoomButton;
+
+    public CruiseDetailsPage(String cruiseName) {
+        prepareGUI(cruiseName);
+    }
+
+    private void prepareGUI(String cruiseName) {
+        detailsFrame = new JFrame("Cruise Details: " + cruiseName);
+        detailsFrame.setSize(800, 600);
+        detailsFrame.setLayout(new BorderLayout());
+
+        cruiseLabel = new JLabel("Cruise Details: " + cruiseName, JLabel.CENTER);
+        detailsFrame.add(cruiseLabel, BorderLayout.NORTH);
+
+        // Sample cruise details
+        //TODO make controller to get cruise details from backend
+        String sampleDetails = "Cruise Name: " + cruiseName + "\n" +
+                "Departure Date: January 1, 2024\n" +
+                "Travel Path: Caribbean, Mediterranean, Alaskan\n" +
+                "Max Capacity: 200 passengers\n" +
+                "Current Occupancy: 100 passengers\n";
+
+        detailsTextArea = new JTextArea(sampleDetails);
+        detailsTextArea.setEditable(false);
+        JScrollPane textScrollPane = new JScrollPane(detailsTextArea);
+        detailsFrame.add(textScrollPane, BorderLayout.CENTER);
+
+        selectRoomButton = new JButton("Select Room");
+        selectRoomButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //TODO Add room selection functionality
+            }
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(selectRoomButton);
+
+        detailsFrame.add(buttonPanel, BorderLayout.SOUTH);
+        detailsFrame.setVisible(true);
+    }
+}
