95cb0fc92b3d20a74627a6daef1a99a92c9be500
Gabriel Choi <gabechoi03@gmail.com>
2023-10-25 04:05:04 -0500

    created login and register account ui

---
+++ Titanic-Project/src/main/java/edu/ui/authentication/LoginPage.java
@@ -0,0 +1,97 @@
+package edu.ui.authentication;
+
+import javax.swing.*;
+import java.awt.*;
+
+import edu.authentication.Authentication;
+import edu.ui.landingPage.LandingPage;
+
+public class LoginPage {
+    private JFrame mainFrame;
+    private JTextField usernameField;
+    private JTextField passwordField;
+
+    public LoginPage(){createGUI();}
+
+    private void createGUI(){
+        mainFrame = new JFrame("Login Page");
+        mainFrame.setSize(1000, 700);
+        mainFrame.setLayout(null);
+
+        JPanel headerPanel = new JPanel();
+        JLabel headerLabel = new JLabel("Login Page", JLabel.CENTER);
+        headerPanel.add(headerLabel);
+        headerPanel.setBounds(400, 0, 200, 30);
+
+        JPanel loginPanel = new JPanel();
+        loginPanel.setBounds(400,300,200,100);
+        loginPanel.setLayout(new GridLayout(3, 2));
+
+        JPanel registerPanel = new JPanel();
+        registerPanel.setBounds(400, 500, 200, 100);
+        registerPanel.setLayout(new GridLayout(2,1));
+        JLabel registerLabel = new JLabel("Don't have an account?", JLabel.CENTER);
+        JButton registerButton = new JButton("Create an account");
+        registerPanel.add(registerLabel);
+        registerPanel.add(registerButton);
+
+        JLabel usernameLabel = new JLabel("Username: ");
+        usernameField = new JTextField(20);
+
+        JLabel passwordLabel = new JLabel("Password: ");
+        passwordField = new JPasswordField(20);
+
+        JButton loginButton = new JButton("Login");
+
+        loginPanel.add(usernameLabel);
+        loginPanel.add(usernameField);
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordField);
+        loginPanel.add(new JLabel());
+        loginPanel.add(loginButton);
+
+        mainFrame.add(headerPanel);
+        mainFrame.add(loginPanel);
+        mainFrame.add(registerPanel);
+
+        loginButton.addActionListener(e -> loginToSystem());
+        registerButton.addActionListener(e -> registerAccount());
+
+        mainFrame.setVisible(true);
+    }
+
+    private void loginToSystem(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        Authentication a = new Authentication();
+
+
+        /*
+
+        Something like this would be used to log user into the system
+
+        if(a.login(username, password)){
+            mainFrame.setVisible(false);
+            new LandingPage();
+        }
+
+         */
+
+        if(username.equals("username") && password.equals("password")){
+            mainFrame.setVisible(false);
+            new LandingPage();
+        }
+
+    }
+
+    private void registerAccount(){
+        mainFrame.setVisible(false);
+        RegisterPage registerPage = new RegisterPage();
+    }
+
+    public static void main(String[] args) {
+        new LoginPage();
+    }
+}
+
---
+++ Titanic-Project/src/main/java/edu/ui/authentication/RegisterPage.java
@@ -0,0 +1,98 @@
+package edu.ui.authentication;
+
+import javax.swing.*;
+import java.awt.*;
+import edu.authentication.Authentication;
+import edu.core.users.User;
+import edu.ui.landingPage.LandingPage;
+
+public class RegisterPage {
+    private JFrame mainFrame;
+    private JTextField usernameField;
+    private JTextField passwordField;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField emailField;
+
+
+    public RegisterPage(){createGUI();}
+
+    public void createGUI(){
+        mainFrame = new JFrame("Create an Account Page");
+        mainFrame.setSize(1000, 700);
+        mainFrame.setLayout(new BorderLayout());
+
+        JLabel registerLabel = new JLabel("Create an Account", JLabel.CENTER);
+        mainFrame.add(registerLabel, BorderLayout.PAGE_START);
+
+        JPanel informationPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(10, 0, 0, 0);
+
+        c.gridx = 0;
+        c.gridy = 0;
+        informationPanel.add(new JLabel("First Name: ", JLabel.CENTER), c);
+        c.gridx = 1;
+        firstNameField = new JTextField(20);
+        informationPanel.add(firstNameField, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        informationPanel.add(new JLabel("Last Name: ", JLabel.CENTER), c);
+        c.gridx = 1;
+        lastNameField = new JTextField(20);
+        informationPanel.add(lastNameField, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        informationPanel.add(new JLabel("Email: ", JLabel.CENTER), c);
+        c.gridx = 1;
+        emailField = new JTextField(20);
+        informationPanel.add(emailField, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        informationPanel.add(new JLabel("Username: ", JLabel.CENTER), c);
+        c.gridx = 1;
+        usernameField = new JTextField(20);
+        informationPanel.add(usernameField, c);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        informationPanel.add(new JLabel("Password: ", JLabel.CENTER), c);
+        c.gridx = 1;
+        passwordField = new JTextField(20);
+        informationPanel.add(passwordField, c);
+
+        JButton registerButton = new JButton("Submit");
+        registerButton.addActionListener(e -> registerAccount());
+        c.gridx = 0;
+        c.gridy = 5;
+        c.gridwidth = 2;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(20, 0, 0, 0);
+        informationPanel.add(registerButton, c);
+
+        mainFrame.add(new JLabel(), BorderLayout.LINE_START);
+        mainFrame.add(informationPanel, BorderLayout.CENTER);
+        mainFrame.add(new JLabel(), BorderLayout.LINE_END);
+
+        mainFrame.setVisible(true);
+    }
+
+    public void registerAccount(){
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String email = emailField.getText();
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        Authentication a = new Authentication();
+
+        a.createAccount(username, password, firstName, lastName, email);
+
+        mainFrame.setVisible(false);
+        LandingPage landingPage = new LandingPage();
+    }
+}
