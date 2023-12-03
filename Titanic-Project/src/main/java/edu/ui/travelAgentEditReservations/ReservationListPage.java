7b2c22859b1a289dc3d5f629941c26705b67a8ea
Cole Hogan <colethogantkd@gmail.com>
2023-12-03 00:08:01 -0600

    commit

--- Titanic-Project/src/main/java/edu/ui/travelAgentEditReservations/ReservationListPage.java
+++ Titanic-Project/src/main/java/edu/ui/travelAgentEditReservations/ReservationListPage.java
@@ -3,9 +3,11 @@ package edu.ui.travelAgentEditReservations;
 import edu.core.cruise.Cruise;
 import edu.core.reservation.Reservation;
 import edu.core.users.Guest;
+import edu.exceptions.NoMatchingReservationException;
 import edu.ui.reservationListInterface.ReservationListInterface;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -81,14 +83,7 @@ public class ReservationListPage implements ReservationListInterface {
             selectButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
             //Check In Button and Check Out Button
-            JButton checkInButton = new JButton("Check In Guest");
 
-
-            selectButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    handleReservationSelection(reservation);
-                }
-            });
             detailsPanel.add(selectButton);
         }
 
@@ -111,13 +106,87 @@ public class ReservationListPage implements ReservationListInterface {
     }
 
 
+    private void checkIn(JTable table) throws NoMatchingReservationException {
+        Reservation r;
+        int selectedRow;
+        selectedRow = table.getSelectedRow();
+        boolean checkedIn = false;
+
+        //check if a row is selected
+        if (selectedRow >= 0) {
+
+            int modelRow = table.convertRowIndexToModel(selectedRow);
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+            //you can't cast the object to an int in case it's null, so you have to cast to string, then cast to int
+            String row = (String) model.getValueAt(modelRow, 0);
+            int intRow = Integer.parseInt(row);
+
+            String userName = String.valueOf(model.getValueAt(intRow, 1));
+            r = ReservationListPageController.getReservation(userName);
+            checkedIn = ReservationListPageController.CheckInGuest(r);
+
+        }
+    }
+
+    private void checkOut(JTable table) {
+        Reservation r;
+        int selectedRow;
+        selectedRow = table.getSelectedRow();
+        boolean checkedIn = false;
+
+        if (selectedRow >= 0) {
+            JFrame frame = new JFrame("Check Out Confirmation");
+            JPanel panel = new JPanel();
+            JButton addButton = new JButton("Add row");
+            addButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+            JLabel confirmation = new JLabel("Are you sure you want to check out:" + r.getUser());
+            confirmation.setAlignmentX(Component.CENTER_ALIGNMENT);
+            panel.add(confirmation);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            int modelRow = table.convertRowIndexToModel(selectedRow);
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+
+            //you can't cast the object to an int in case it's null, so you have to cast to string, then cast to int
+            String row = (String) model.getValueAt(modelRow, 0);
+            int intRow = Integer.parseInt(row);
+
+            String userName = String.valueOf(model.getValueAt(intRow, 1));
+            JButton checkOutButton = new JButton("Yes Check Out");
+            checkOutButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ReservationListPageController.CheckOutGuest(r);
+                    frame.dispose();
+                }
+            });
+            panel.add(checkOutButton);
+
+            JButton closeButton = new JButton("no");
+            closeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    frame.dispose();
+                }
+            });
+
+            frame.add(panel);
+            frame.add(closeButton);
+            frame.setLocationRelativeTo(null);
+
+            frame.setVisible(true);
+
+        }
+    }
 
 
     private void handleReservationSelection(Reservation reservation) {
         mainFrame.setVisible(false);
         new EditReservationPage(this, reservation.getRoom().getCruise(), reservation);
     }
-
 
 
     private void refreshData() {
@@ -129,4 +198,5 @@ public class ReservationListPage implements ReservationListInterface {
 
         refreshData();
     }
+
 }
