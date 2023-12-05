4c1e1cb4af33e7526c07fd272e6f67b937da8eff
Cole Hogan <colethogantkd@gmail.com>
2023-12-05 07:58:31 -0600

    merging with main

--- Titanic-Project/src/main/java/edu/databaseAccessors/driver.java
+++ Titanic-Project/src/main/java/edu/databaseAccessors/driver.java
@@ -4,19 +4,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public interface driver {
+//public interface driver {
 
-    public static Connection getDBConnection() {
-        String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-        String url = "jdbc:derby:C:\\Users\\Colet\\Documents\\GIT\\Titanic2.0\\Titanic-Project\\src\\main\\java\\edu\\database";
-        Connection dbConnection = null;
-        //Class.forName(DB_DRIVER);
-        try {
-            dbConnection = DriverManager.getConnection(url);
-            return dbConnection;
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return dbConnection;
-    }
-}
+//    public static Connection getDBConnection() {
+//        String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+//        String url = "jdbc:derby:C:\\Users\\Colet\\Documents\\GIT\\Titanic2.0\\Titanic-Project\\src\\main\\java\\edu\\database";
+//        Connection dbConnection = null;
+//        //Class.forName(DB_DRIVER);
+//        try {
+//            dbConnection = DriverManager.getConnection(url);
+//            return dbConnection;
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return dbConnection;
+//    }
+//}
